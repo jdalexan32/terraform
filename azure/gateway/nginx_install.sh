cfd0fde6fba8015a3af593351b607743db28a2b1
Jay Alexander <jdalexan32@gmail.com>
2021-07-18 11:30:57 +0300

    removed upstream server info

--- azure/gateway/nginx_install.sh
+++ azure/gateway/nginx_install.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 sudo apt-get -y update
-sudo apt-get -y install nginx               # <----------------- EDIT BELOW SERVER DATA ------------------
+sudo apt-get -y install nginx       # <----------------- EDIT BELOW SERVER DATA Replace <> WITH YOUR DATA ------------------
 echo '
 stream {
-    upstream NAME { 
-        server PATH:PORT;
+    upstream <NAME> { 
+        server <PATH:PORT>;
         }
         server {
             listen 3306;
-            proxy_pass NAME;
+            proxy_pass <NAME>;
         }
     }' >> /etc/nginx/nginx.conf
 sudo systemctl restart nginx
